08219213916c8400499279a957ce20a9f9a24391
Kirill Vorobyev <kirill.vorobyev.313@gmail.com>
2018-11-13 20:20:11 -0500

    added monster shapes and health numbers

--- main.js
+++ main.js
@@ -15,13 +15,14 @@ function makeMonster(){
 }
 
 function makeHealthBarContainer(){
-    let healthBarContainer = $('<div/>').attr({id:'healthContainer','health':20,'maxHealth':20});
+    let health = 20;
+    let healthBarContainer = $('<div/>').attr({id:'healthContainer','health':health,'maxHealth':health}).text(health+'/'+health);
     return healthBarContainer;
 }
 
 function takeDamage(monster,healthBar){
     changeMonsterColor(monster);
-    changeMonsterHealth(healthBar);
+    changeMonsterHealth(healthBar,monster);
 }
 
 function changeMonsterColor(monster){
@@ -29,14 +30,27 @@ function changeMonsterColor(monster){
     monster.css({'background-color': 'rgb('+randomBrightness+',0,'+randomBrightness+')'});
 }
 
-function changeMonsterHealth(healthBar){
+function changeMonsterShape(monster){
+    let borders = '';
+    for(let i=1;i<=4;i++){
+        borders += Math.random()*350+10 + 'px ';
+    }
+    monster.css({'border-radius': borders});
+}
+
+function changeMonsterHealth(healthBar,monster){
     let health = healthBar.attr('health');
     let maxHealth = healthBar.attr('maxHealth');
     health-=1;
     if(health==0){
-        health = 20;
+        health = Math.round(Math.random()*33+7);
+        maxHealth = health
+        healthBar.attr({'health':health, 'maxHealth':maxHealth});
+        changeMonsterShape(monster);
+    }else{
+        healthBar.attr({'health':health});
     }
-    healthBar.attr({'health':health});
+    healthBar.text(health+'/'+maxHealth);
     let healthPercentage = health/maxHealth * 100;
     healthBar.css({'background-image': 'linear-gradient(90deg, red '+healthPercentage+'%, #ddd '+healthPercentage+'%)'});
 }
